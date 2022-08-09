7f571702a7e90809f196cc8db50a67ef6598a5ec
sterling#elysian.JP60=={56} <shohan.sub.56@gmail.com>
2022-08-10 01:59:59 +0600

    done

---
+++ js/script.js
@@ -0,0 +1,29 @@
+// mobile nav open or hide
+const navButton = document.querySelector(".btn-mobile-nav");
+const header = document.querySelector(".header");
+
+navButton.addEventListener("click", () => {
+  header.classList.toggle("nav-open");
+});
+
+//navbar sticky events
+const sectionHero = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    if (!ent.isIntersecting) {
+      document.body.classList.add("sticky");
+    }
+    if (ent.isIntersecting) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+
+obs.observe(sectionHero);
